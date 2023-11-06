22c1d0c4a4d71073198df3015e30d8cff523eeef
wtshek <wingtung.shek@accedo.tv>
2023-11-06 14:44:28 -0500

    create database schema

---
+++ database/migrations/2023_11_06_194343_create_jobs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('role');
+            $table->integer('min_salary');
+            $table->integer('max_salary');
+            $table->string('location');
+            $table->longText('descriptions');
+            $table->boolean('remote');
+            $table->foreignId('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
